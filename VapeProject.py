54500943bd7ef4654fe08f3b6209f28e2878bba5
Mark Gorelik <mark.grg@gmail.com>
2017-06-29 12:50:13 -0500

    Add files via upload

---
+++ VapeProject.py
@@ -0,0 +1,166 @@
+from bs4 import BeautifulSoup
+import os
+import operator
+import praw
+import time
+d = ""
+o = ""
+uAgent = ""
+
+bot = praw.Reddit(user_agent = uAgent, client_secret ="", client_id="", usename="");
+searchSubs = ['vapeheads', 'ShakeAndVape', 'vaping101', 'juiceswap', 'EJuicePorn']
+
+for i in searchSubs:
+    subreddit = bot.subreddit(i)
+    name = "output"+i+".csv"
+    output = open(name, "w")
+    for submission in subreddit.submissions():
+        submission.comments.replace_more(limit=0)
+        for comment in submission.comments.list():
+            print(comment.body)
+            s = str(comment.body)
+            s.strip(",")
+            output.write(s+",")
+    output.close()
+        
+
+
+def collect_files(directory):
+    files = []
+    f = open(directory)
+    for line in f:
+        files.append(line)
+
+    return files
+
+def clean_text(s: str):
+    s = s.lower()
+    t=""
+    for i in s:
+        if i in "qwertyuiopasdfghjklzxcvbnm ":
+            t += i
+    return t
+
+class Scraper:
+    """docstring for Scraper"""
+    def __init__(self, directory: str, outputfile: str):
+        print("Started")
+        
+        self.directory = directory
+        self.outputfile = outputfile
+        self.flavors = []
+
+    def scanner(self):
+        print("scanning")
+        files = collect_files(self.directory)
+
+        for file in files:
+            try:
+                soup = BeautifulSoup(open("flavors"+file[1:-1]))
+                print(soup)
+                for link in soup.find_all("a"):
+
+                    if "flavor" in link.get("href"):
+                        print(link)
+                        self.flavors.append(link.text)
+                        print(link.text)
+            except:
+                pass
+
+
+    def save(self):
+        print("Saving")
+        f = open(self.outputfile, "w")
+        for i in self.flavors:
+            print(i)
+            f.write(i+"\n")
+        f.close()
+
+
+
+
+def cleaner(inp: str, output: str):
+    f = open(inp)
+    o = open(output, "w+")
+    for line in f:
+        t = ""
+        line = line.lower()
+
+
+        for i in line:
+            if i in "qwertyuiopasdfghjklzxcvbnm ":
+                t+=i
+        if t == "" or t == " ":
+            pass
+        else:
+            o.write(t+"\n")
+    f.close()
+    o.close()
+
+
+class Recipe_scraper:
+    """docstring for Scraper"""
+    def __init__(self, directory: str, outputfile: str):
+        print("Started")
+        
+        self.directory = directory
+        self.outputfile = outputfile
+        self.flavors = {}
+
+    def scanner(self):
+
+        files = collect_files(self.directory)
+        for file in files:
+            file = file.strip("\n")
+            f = open(file)
+            try:
+
+                soup = BeautifulSoup(open(file), "lxml") 
+                for link in soup.find_all("a"):
+
+                    if "flavor" in link.get("href"):
+                        t = link.text
+                        t = clean_text(t)
+                        if t in self.flavors.keys():
+                            self.flavors[t] = self.flavors[t]+1
+                        else:
+                            self.flavors[t] = 1
+
+                         
+            except:
+                pass
+
+        self.flavors = sorted(self.flavors.items(), key=operator.itemgetter(1))
+        print(self.flavors)
+    def save(self):
+        print("Saving")
+        f = open(self.outputfile, "w")
+        for i in self.flavors:
+            print(i)
+            f.write(str(i)+"\n")
+        f.close()
+
+
+
+
+def cleaner(inp: str, output: str):
+    f = open(inp)
+    o = open(output, "w+")
+    for line in f:
+        t = ""
+        line = line.lower()
+
+
+        for i in line:
+            if i in "qwertyuiopasdfghjklzxcvbnm ":
+                t+=i
+        if t == "" or t == " ":
+            pass
+        else:
+            o.write(t+"\n")
+    f.close()
+    o.close()
+
+x = Recipe_scraper(d, o)
+x.scanner()
+x.save()
---
+++ reddit.py
@@ -0,0 +1,92 @@
+from pathlib import Path
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+import glob
+import csv
+import string
+import random
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import nltk
+from nltk.corpus import movie_reviews
+from nltk.stem import WordNetLemmatizer
+sentences = []
+sw = stopwords.words("english")
+
+def clean_text(t):
+    t= nltk.word_tokenize(t, language='english')
+    c_t = []
+    for i in t:
+        if i not in "!@#$%^&*()_+.,></?'/":
+            c_t.append(i)
+    t = []
+    lemmatizer = WordNetLemmatizer()
+    for i in c_t:
+        #add stop word filtering
+        t.append(lemmatizer.lemmatize(i))
+    c_t = " ".join(t)
+    
+    return c_t
+print(clean_text("An entertained pulp posts the gospel underneath the no coast."))
+
+
+
+documents = [(list(movie_reviews.words(fileid)), category)
+             for category in movie_reviews.categories()
+             for fileid in movie_reviews.fileids(category)]
+print("loaded docs")
+random.shuffle(documents)
+x_train = []
+y_train = []
+
+for i in range(len(documents)):
+    if documents[i][1] == "neg":
+        y_train.append(0)
+        text = " ".join(documents[i][0])
+        text = nltk.word_tokenize(text, language='english')
+        x_train.append(clean_text(" ".join(documents[i][0])))
+
+    else:
+        y_train.append(1)
+        x_train.append(clean_text(" ".join(documents[i][0])))
+print("prepped docs")
+vectorizer = CountVectorizer(analyzer = "word", max_features = 5000, tokenizer = None, stop_words = None, preprocessor = None) 
+x_train = vectorizer.fit_transform(x_train)
+x_train = x_train.toarray()
+classy = MultinomialNB()
+classy.fit(x_train, y_train)
+print("trained")
+reddit = []
+f = open("clean2.txt")
+for line in f:
+    line = line.lower()
+    line = clean_text(line)
+    reddit.append(line)
+print("loaded reddit docs")
+t_reddit = reddit
+reddit = vectorizer.fit_transform(reddit)
+reddit = reddit.toarray()
+result = classy.predict(reddit)
+print("Predicted reddit docs")
+
+flavors = open("/Users/mark/Desktop/co.txt")
+flavor_list = []
+for i in flavors:
+    i = clean_text(i)
+    flavor_list.append(i)
+flavor_results = {}
+for i in range(len(t_reddit)):
+    for j in flavor_list:
+        if j in t_reddit[i]:
+            if j in flavor_results.keys():
+                if result[i] == 0:
+                    flavor_results[j] = [flavor_results[j][0], flavor_results[j][1]+1]
+                else:
+                    flavor_results[j] = [flavor_results[j][0]+1, flavor_results[j][1]]
+            else:
+                if result[i] == 0:
+                    flavor_results[j] = [0, 1]
+                else:
+                    flavor_results[j] = [1, 0]
+print(flavor_results)
+
